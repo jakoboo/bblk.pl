dd0fa13a3ffb3281308f9caf7d40f0a37da5d53f
Jakub <jbbabelek@gmail.com>
2020-10-14 18:14:53 +0200

    wip

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,7 +45,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return;
   }
 
-  const posts = result.data.allMarkdownRemark.nodes;
+  const posts = result.data.allMdx.nodes;
 
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
